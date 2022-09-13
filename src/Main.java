5c2cbd4c6f075be8d98ef9ab82719f9eb0ef50f6
Maxim <vedernikovmaxim@rambler.ru>
2022-09-13 21:07:46 +0300

    Add logger

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+
 public class Main {
-    public static void main(String[] args) {
+    private static final Logger log = Logger.getLogger(Main.class);
+    public static void main(String[] args) throws IOException {
+
+        log.debug("Start");
         App run = new App();
         run.start();
+
     }
 }
